3f9fb8960b723f8765f3247aebe1c85166f95f27
Lukas <lukas.tabulevicius@gmail.com>
2022-04-21 10:53:53 +0300

    js initial

---
+++ js/components/features/features.js
@@ -0,0 +1,26 @@
+function isValidSelevtor(params) {
+    if (typeof selector !== 'string') {
+        return [true, 'Selector turi buti stringas'];
+    }
+    if selector === "" {
+        return [true, 'Selector turi buti ne tuscias stringas'];
+    }
+    return [false, 'OK'];
+}
+
+renderFeatures(selector, data) {
+    console.log(list);
+}
+
+let selectorRes = isValidSelevtor(selector);
+if (selectorRes[0]) {
+    return selectorl
+}
+
+const DOM = document.querySelector(selector);
+if (!DOM) {
+    return [true, 'Pagal pateikta selector nerasta elementu']
+}
+
+export {renderFeatures};
+
--- js/pages/home.js
+++ js/pages/home.js
@@ -1,3 +1,60 @@
 // component imports
 
-// component executions
+import {renderfeatures} from '../components/features/features.js';
+
+// components execution
+
+/* header: start */
+/* header: end */
+
+/* hero: start */
+/* hero: end */
+
+/* premium features: start */
+
+
+let featuresData = [
+    {
+        icon: '',
+        title: '',
+        desciption: '',
+    }
+]
+
+/* premium features: end */
+
+/* comunication app: start */
+/* comunication app: end */
+
+/* device manage: start */
+/* device manage: end */
+
+/* how app works: start */
+/* how app works: end */
+
+/* interface slide: start */
+/* interface slide: end */
+
+/* testimonials: start */
+/* testimonials: end */
+
+/* pricing: start */
+/* pricing: end */
+
+/* FAQ: start */
+/* FAQ: end */
+
+/* team: start */
+/* team: end */
+
+/* app store: start */
+/* app store: end */
+
+/* subscribe: start */
+/* subscribe: end */
+
+/* contacts: start */
+/* contacts: end */
+
+/* footer: start */
+/* footer: end */
